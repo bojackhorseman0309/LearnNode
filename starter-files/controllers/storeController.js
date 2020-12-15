3c88fabd994483019252f23ec9fd18b307444249
Alonso Araya Calvo <alonso@elumen.info>
2020-12-15 13:03:20 -0600

    LN-4 Adding Module 4 and 5

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,9 @@
+const { response } = require('express');
+const mongoose = require('mongoose');
+const { options } = require('../routes');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 };
 
@@ -7,6 +11,36 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  req.flash(
+    'success',
+    `Successfully Created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id });
+
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
+
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true,
+  }).exec();
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}>View Store</a>`
+  );
+  res.redirect(`/stores/${store._id}/edit`);
 };
