70c2dd1c48d19231c2e9171f9de21b8500565d16
Alonso Araya Calvo <alonso@elumen.info>
2020-12-15 10:10:24 -0600

    LN-3 Adding Module 3 code

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,9 +1,12 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Alonso';
-  next();
-};
-
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
 };
