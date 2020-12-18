6aa8d2b1ab6c13c79d85edad5eff30ad07c6f086
bojackHorseman0309 <aloaraya@outlook.com>
2020-12-17 20:02:15 -0600

    LN-8 Adding Module 9

--- starter-files/controllers/authController.js
+++ starter-files/controllers/authController.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -39,10 +40,14 @@ exports.forgot = async (req, res) => {
   await user.save();
 
   const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-  req.flash(
-    'success',
-    `You have been emailed a password reset link. ${resetURL}`
-  );
+
+  await mail.send({
+    user: user,
+    subject: 'Password Reset',
+    resetURL: resetURL,
+    filename: 'password-reset',
+  });
+  req.flash('success', `You have been emailed a password reset link.`);
   res.redirect('/login');
 };
 
