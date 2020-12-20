4bc5a21a0042931be54bea3a9fcc7fa36d86cd45
bojackHorseman0309 <aloaraya@outlook.com>
2020-12-20 13:52:25 -0600

    Adding Pagination

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -57,8 +57,30 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
-  const stores = await Store.find();
-  res.render('stores', { title: 'Stores', stores });
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = page * limit - limit;
+
+  const storesPromise = Store.find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: 'desc' });
+
+  const countPromise = Store.count();
+
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+  const pages = Math.ceil(count / limit);
+
+  if (!stores.length && skip) {
+    req.flash(
+      'info',
+      `Hey you asked for page ${page} but that doesn't exist. So i put you in on page ${pages}`
+    );
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+  res.render('stores', { title: 'Stores', stores, page, pages, count });
 };
 
 const confirmOwner = (store, user) => {
